8393864c00f6d95ecaf7089b85bf929fd93f540f
maskedpirate <13569996+maskedpirate@users.noreply.github.com>
2026-08-02 10:08:03 -0400

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,13 +24,13 @@ fi
 sudo git clone "$REPO_URL" "$REPO_DIR"
 
 # Transfer ownership of the cloned folder back to your current user
-# This prevents permission errors when Ansible tries to read it
 sudo chown -R "$USER:$USER" "$REPO_DIR"
 
 echo "Running Ansible Playbook..."
 cd "$REPO_DIR"
 
-# Run the playbook locally
-ansible-playbook -i localhost, -c local playbook.yml
+# Run the playbook as root so Ansible doesn't prompt for a become password
+# (The playbook will automatically detect your original user via $SUDO_USER)
+sudo ansible-playbook -i localhost, -c local playbook.yml
 
 echo "Bootstrap complete! You can now create your cluster with 'kind create cluster'."
